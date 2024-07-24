1ee7014b8e1032cfbdd4dd2f9844986d6e288258
Alona Maksymova <alonamaksymova5@gmail.com>
2024-07-24 12:17:29 +0300

    change task

---
+++ src/js/2-form.js
@@ -0,0 +1,60 @@
+let formData = {
+    email: "",
+    message: ""
+}
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+populateForm();
+
+function handleInput(event) {
+    const value = event.target.value.trim();
+    const key = event.target.name;
+    formData[key] = value;
+
+    try {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    } catch (err) {
+    console.error(err);
+    return;
+    }
+}
+
+function populateForm() {
+    try {
+    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (savedData) {
+        formData = savedData;
+        for (const key in formData) {
+        if (formData[key]) {
+            form.elements[key].value = formData[key];
+        }
+        }
+    }
+    } catch (err) {
+    console.error(err);
+    return;
+    }
+}
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    if (
+        !form.elements.email.value.trim() ||
+        !form.elements.message.value.trim()
+    ) {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log('Form data submitted:', formData);
+
+    localStorage.removeItem('feedback-form-state');
+    formData = { email: '', message: '' };
+    form.reset();
+}
